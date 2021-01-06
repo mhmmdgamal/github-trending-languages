d66b3ea8634f699f0e6adf920bfe17401e9a077b
Mohamed Gamal <mohamed.gamal@e-masary.com>
2021-01-07 01:18:49 +0200

    refactor imports

--- src/test/java/com/gemography/githubtrendinglanguages/service/TrendingLanguagesServiceTests.java
+++ src/test/java/com/gemography/githubtrendinglanguages/service/TrendingLanguagesServiceTests.java
@@ -13,7 +13,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.HttpStatus.OK;
 
