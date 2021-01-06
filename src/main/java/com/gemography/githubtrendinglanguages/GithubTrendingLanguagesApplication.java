ffde5d025f97aae7f98517167f520bae3bd09258
Mohamed Gamal <mohamed.gamal@e-masary.com>
2021-01-06 14:33:00 +0200

    App initialize version

---
+++ src/main/java/com/gemography/githubtrendinglanguages/GithubTrendingLanguagesApplication.java
@@ -0,0 +1,13 @@
+package com.gemography.githubtrendinglanguages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubTrendingLanguagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubTrendingLanguagesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gemography/githubtrendinglanguages/GithubTrendingLanguagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gemography.githubtrendinglanguages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubTrendingLanguagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
