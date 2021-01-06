2f31aed604f9cec3027583944cdf0411f3100f9c
Mohamed Gamal <mohamed.gamal@e-masary.com>
2021-01-06 20:07:39 +0200

    Add service Exception and handle ProviderGeneralException

---
+++ src/main/java/com/gemography/githubtrendinglanguages/service/ExceptionService.java
@@ -0,0 +1,21 @@
+package com.gemography.githubtrendinglanguages.service;
+
+import com.gemography.githubtrendinglanguages.dto.TrendingLanguagesDto;
+import com.gemography.githubtrendinglanguages.provider.exception.ProviderGeneralException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Arrays;
+
+import static org.springframework.http.HttpStatus.SERVICE_UNAVAILABLE;
+
+@RestControllerAdvice
+public class ExceptionService {
+    @ExceptionHandler(value = ProviderGeneralException.class)
+    public ResponseEntity<TrendingLanguagesDto> handleProviderGeneralException(ProviderGeneralException ex) {
+        return ResponseEntity
+                .status(SERVICE_UNAVAILABLE)
+                .body(new TrendingLanguagesDto(SERVICE_UNAVAILABLE.value(), Arrays.asList()));
+    }
+}
