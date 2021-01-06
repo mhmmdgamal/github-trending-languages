d188291ed8300a5bda4cbf2ed1ced7bce5654400
Mohamed Gamal <mohamed.gamal@e-masary.com>
2021-01-06 19:47:37 +0200

    refactor TrendingLanguagesService -Use Slide Statements -Use Extract Function -Use Rename Variable

--- src/main/java/com/gemography/githubtrendinglanguages/dto/LanguageDto.java
+++ src/main/java/com/gemography/githubtrendinglanguages/dto/LanguageDto.java
@@ -3,12 +3,16 @@ package com.gemography.githubtrendinglanguages.dto;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
 @Setter
 @ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class LanguageDto {
     private String name;
     private Integer count;
--- src/main/java/com/gemography/githubtrendinglanguages/service/TrendingLanguagesService.java
+++ src/main/java/com/gemography/githubtrendinglanguages/service/TrendingLanguagesService.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 @Service
@@ -18,29 +20,44 @@ public class TrendingLanguagesService {
     private final TrendingReposConsumer trendingReposConsumer;
 
     public TrendingLanguagesDto fetchTrendingLanguages() {
-        Map<String, List<ProviderTrendingRepoDto>> groupedLanguages =
-                trendingReposConsumer.fetchTrendingRepos()
-                        .getProviderTrendingRepos()
-                        .stream()
-                        .filter(providerTrendingRepo -> providerTrendingRepo.getLanguage() != null)
-                        .collect(Collectors.groupingBy(providerTrendingRepo -> providerTrendingRepo.getLanguage()));
+        return new TrendingLanguagesDto(200, getLanguages(getGroupedLanguages()));
+    }
 
-        List<LanguageDto> languageDtoList =
-                groupedLanguages.keySet().stream()
-                        .map(language -> {
-                            List<RepositoryDto> repositories = groupedLanguages.get(language)
-                                            .stream()
-                                            .map(RepositoryDto::new)
-                                            .collect(Collectors.toList());
+    private Map<String, List<ProviderTrendingRepoDto>> getGroupedLanguages() {
+        return trendingReposConsumer.fetchTrendingRepos()
+                .getProviderTrendingRepos()
+                .stream()
+                .filter(filterNotFoundLanguages())
+                .collect(Collectors.groupingBy(groupByLanguages()));
+    }
 
-                            LanguageDto languageDto = new LanguageDto();
-                            languageDto.setCount(groupedLanguages.get(language).size());
-                            languageDto.setName(language);
-                            languageDto.setRepositories(repositories);
-                            return languageDto;
-                        })
-                        .collect(Collectors.toList());
+    private Predicate<ProviderTrendingRepoDto> filterNotFoundLanguages() {
+        return providerTrendingRepo ->
+                providerTrendingRepo.getLanguage() != null && !providerTrendingRepo.getLanguage().isEmpty();
+    }
 
-        return new TrendingLanguagesDto(200, languageDtoList);
+    private Function<ProviderTrendingRepoDto, String> groupByLanguages() {
+        return providerTrendingRepo -> providerTrendingRepo.getLanguage();
+    }
+
+    private List<LanguageDto> getLanguages(Map<String, List<ProviderTrendingRepoDto>> groupedLanguages) {
+        return groupedLanguages.keySet().stream()
+                .map(mapToLanguageDto(groupedLanguages))
+                .collect(Collectors.toList());
+    }
+
+    private Function<String, LanguageDto> mapToLanguageDto(Map<String, List<ProviderTrendingRepoDto>> groupedLanguages) {
+        return language -> buildLanguageDto(groupedLanguages, language, getRepositories(groupedLanguages, language));
+    }
+
+    private List<RepositoryDto> getRepositories(Map<String, List<ProviderTrendingRepoDto>> groupedLanguages, String language) {
+        return groupedLanguages.get(language)
+                .stream()
+                .map(RepositoryDto::new)
+                .collect(Collectors.toList());
+    }
+
+    private LanguageDto buildLanguageDto(Map<String, List<ProviderTrendingRepoDto>> groupedLanguages, String language, List<RepositoryDto> repositories) {
+        return new LanguageDto(language, groupedLanguages.get(language).size(), repositories);
     }
 }
